2f3ff73fadcf6ef599001ff4a73887e4e8a7c384
OSIS tech 27 <koordinatordaerahwilayahxi@gmail.com>
2026-08-28 20:54:51 +0700

    seed.sql

---
+++ supabase/seed.sql
@@ -0,0 +1,218 @@
+insert into public.participants
+(
+  name,
+  participant_number,
+  class,
+  status,
+  stage,
+  is_published
+)
+values
+
+-- X M1
+
+('Alana Rachelia Putri', 'KM26-001', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Arum Aprianti', 'KM26-002', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Shifa', 'KM26-003', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Salisa Nayla Ilmi', 'KM26-004', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Ilham Setiawan', 'KM26-005', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Fachrijal Rohanda Putra', 'KM26-006', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Meisya Queennita Jim Jim', 'KM26-007', 'X M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- X M2
+
+('Rasyifa Nur Azizah', 'KM26-008', 'X M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Diva Ramianti', 'KM26-009', 'X M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Nura Nurjanah', 'KM26-010', 'X M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Putri Dewi Rahmawati', 'KM26-011', 'X M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Vinisa Dwi Wardani', 'KM26-012', 'X M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- X M3
+
+('Kirana Sakila Ayu', 'KM26-013', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Ria Amelia', 'KM26-014', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Gibran Pareja', 'KM26-015', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rayhan Dzul Hilmi', 'KM26-016', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Sani', 'KM26-017', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Bisma Erlangga', 'KM26-018', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Azis M. Syah Putra', 'KM26-019', 'X M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- X M4
+
+('M. Bagas Al Fauzan', 'KM26-020', 'X M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Qotrun Nada Hamidah', 'KM26-021', 'X M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Silvia Nur Alisa Bela', 'KM26-022', 'X M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Putri Nurmala Sari', 'KM26-023', 'X M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- X M5
+
+('Gesit Zahrotutshita Prasasti', 'KM26-024', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Meisya Almahera', 'KM26-025', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Mega Sinta Mustika', 'KM26-026', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Nadia', 'KM26-027', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Nurul Pradina Putri', 'KM26-028', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Adiansyah', 'KM26-029', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rafa Tian Ramadani', 'KM26-030', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Yeza Rocky Ardian', 'KM26-031', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Azis Alfiansyah', 'KM26-032', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Abdul Rohman', 'KM26-033', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Sani Saputra', 'KM26-034', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Fadlin Ramdhani', 'KM26-035', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rikma Firmansyah', 'KM26-036', 'X M5', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- XI M1
+
+('Fika Rahma Arnida', 'KM26-037', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Raya Fazria', 'KM26-038', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Garnisa Novianti', 'KM26-039', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Garshela Putri Deris', 'KM26-040', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Revalina Agustina', 'KM26-041', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Amira Ambar Ramadhani', 'KM26-042', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Alkha Pramudya', 'KM26-043', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Zahwa Kanesha Ayudia Dewani', 'KM26-044', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Saira Aulia', 'KM26-045', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Pani Putri Utami', 'KM26-046', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Lia Apriliani', 'KM26-047', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Saepul Jamil', 'KM26-048', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Ezar Arafi Setia Murni', 'KM26-049', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Dude Harlino', 'KM26-050', 'XI M1', 'BELUM_DIUMKAN', 'Pendaftaran', false),
+('Darul Ibnu Muzakki', 'KM26-051', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rifki Salam', 'KM26-052', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('M. Ripqi al paqih', 'KM26-053', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Bayu Firmansyah', 'KM26-054', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Tio Setiawan', 'KM26-055', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rahmi Maulida', 'KM26-056', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Fasha', 'KM26-057', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Ringga Satria', 'KM26-058', 'XI M1', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- XI M2
+
+('Dinda Lurina', 'KM26-059', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rivaldo Aulia', 'KM26-060', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Resti Pitriani', 'KM26-061', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rina Yusriani', 'KM26-062', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Cindy Natalia Octavia', 'KM26-063', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Nida Awalia', 'KM26-064', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Zaskia Natasya', 'KM26-065', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Dini Sri Rahayu', 'KM26-066', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Savina Maulida', 'KM26-067', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Nuri Samsifa', 'KM26-068', 'XI M2', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- XI M3
+
+('Zira Almeira', 'KM26-069', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Yassa Putra Yogiansyah', 'KM26-070', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Gesya Alivina', 'KM26-071', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rizky Ramadhan', 'KM26-072', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Dewi Rahma', 'KM26-073', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Dinda Sindraina', 'KM26-074', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Meti Purnama Sari C', 'KM26-075', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Refa Cahyani', 'KM26-076', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Vikka Alnabilla', 'KM26-077', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Hilal Fauzi', 'KM26-078', 'XI M3', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+
+-- XI M4
+
+('Adi Mulyana', 'KM26-079', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rafael Hidayatullah', 'KM26-080', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Rafka Adi Putra', 'KM26-081', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Kisti Melistriani', 'KM26-082', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Putri Ustika Cahyani', 'KM26-083', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false),
+('Intan', 'KM26-084', 'XI M4', 'BELUM_DIUMUMKAN', 'Pendaftaran', false)
+
+on conflict (participant_number)
+do nothing;
+
+
+-- ============================================
+-- TIMELINE
+-- ============================================
+
+insert into public.selection_stages
+(
+  order_number,
+  name,
+  description,
+  status
+)
+values
+
+(
+  1,
+  'Pendaftaran',
+  'Tahap pendaftaran calon pengurus OSIS.',
+  'SELESAI'
+),
+
+(
+  2,
+  'Seleksi Tahap 1',
+  'Seleksi awal calon pengurus.',
+  'AKAN_DATANG'
+),
+
+(
+  3,
+  'Test Tulis',
+  'Tes tertulis untuk mengetahui kemampuan peserta.',
+  'AKAN_DATANG'
+),
+
+(
+  4,
+  'Wawancara',
+  'Wawancara peserta bersama tim seleksi.',
+  'AKAN_DATANG'
+),
+
+(
+  5,
+  'Pengumuman',
+  'Pengumuman hasil setiap tahapan seleksi.',
+  'AKAN_DATANG'
+),
+
+(
+  6,
+  'Debat Tertutup',
+  'Debat tertutup Caketos dan Waketos terpilih, masing-masing 10 orang.',
+  'AKAN_DATANG'
+),
+
+(
+  7,
+  'Verifikasi Visi Misi',
+  'Verifikasi visi dan misi calon.',
+  'AKAN_DATANG'
+),
+
+(
+  8,
+  'Debat Terbuka',
+  'Debat terbuka calon Ketua dan Wakil Ketua OSIS.',
+  'AKAN_DATANG'
+),
+
+(
+  9,
+  'Pemilihan OSIS / KPO',
+  'Pemilihan Ketua dan Wakil Ketua OSIS.',
+  'AKAN_DATANG'
+);
+
+-- ============================================
+-- PENGUMUMAN AWAL
+-- ============================================
+
+insert into public.announcements
+(
+  title,
+  content,
+  is_published
+)
+values
+(
+  'Selamat Datang di Karsa Muda Selection 2026',
+  'Portal resmi hasil seleksi calon pengurus OSIS periode 2026/2027. Silakan gunakan nomor peserta untuk mengecek hasil setelah panitia melakukan publikasi.',
+  true
+);
